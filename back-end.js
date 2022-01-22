bf555a4646074390901765fab029c5f6e28bcda2
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-21 23:25:38 -0300

    Aula 1 - Javascript para Back-end

--- back-end.js
+++ back-end.js
@@ -1,9 +1,48 @@
-const  meuNumero = 3
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
 
-const  primeiroNumero = 2
-const  segundoNumero = 1
+const citacao = "Meu nome é ";
+const meuNome = "Thiago";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
 
 
-const operacaoMatematica = primeiroNumero + segundoNumero;
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
+
+
+
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
 
 console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
