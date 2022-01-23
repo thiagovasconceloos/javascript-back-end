de5f0b3e6d89c433f9d1e81c95abb1e1aed8dd99
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-22 21:33:53 -0300

    Aula 6 Javascript para Back end

---
+++ strings.js
@@ -0,0 +1,15 @@
+const meuNome = "Thiago Vasconcelos"
+const idade = 2022 - 1995;
+const cidade = "Jundiaí"
+
+
+// const apresentacao = "Meu nome é " + " " + meuNome + " tenho " 
+// + idade + " anos," + " nasci em " + cidade 
+
+
+
+const apresentacao =    `Meu nome é ${meuNome}, minha idade é ${idade}, e nasci na cidade de ${cidade}`;
+
+
+
+console.log(apresentacao)
