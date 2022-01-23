3750beda16ad0701f59df57fc66e85654bcca811
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-22 21:16:37 -0300

    Aula 4 Javascript para Back-end

---
+++ operadores.js
@@ -0,0 +1,11 @@
+// == faz a comparacao ja convertendo o tipo 
+// === faz a comparação validando o tipo e valor 
+
+
+const numero = 5;
+const texto = "5"
+
+console.log(numero==texto)
+console.log(numero===texto)
+
+ console.log (typeof numero + "\n"  + typeof texto)
---
+++ ternarios.js
@@ -0,0 +1,16 @@
+const idadeMinima = 18
+const idadeCliente = 16;
+
+
+// if(idadeCliente >= idadeMinima){
+//    console.log("Permitido");
+
+
+     
+// }else {
+
+//     console.log("Voce não pode beber.")
+// }
+
+ // condição                                //true            //false
+console.log(idadeCliente>= idadeMinima ? "Permitido" : "Você não pode beber");
