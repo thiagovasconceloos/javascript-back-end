1401543aaee4c6e4a9e932a18f30324210a8d13e
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-23 15:50:55 -0300

    Finalizando  o curso

---
+++ expressoes.js
@@ -0,0 +1,5 @@
+
+
+const soma = function(num1,num2 ){ return num1 + num2}
+
+console.log(soma(2,3))
---
+++ funcoes.js
@@ -0,0 +1,23 @@
+
+
+
+function imprimeTexto (texto) {
+      console.log(texto)
+
+
+}
+
+imprimeTexto('Teste');
+
+imprimeTexto('Testando a função')
+
+function somaValores(x,b){
+
+    return x + b ;
+
+
+}
+
+console.log(somaValores(10,54))
+
+
